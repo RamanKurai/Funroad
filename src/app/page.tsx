647150444cfc39eb7eef50e8936f0466209624f2
Raman Kurai <ramankurai27@gmail.com>
2025-07-09 21:33:31 +0530

    style: update border and input colors for consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import React from "react";
 
 const Homepage = () => {
   return (
-    <div>
-     
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant={"elevated"}>Iam a Button</Button>
+        </div>
+        <div>
+          <Input placeholder="Iam an Input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="Iam a Text Area" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
