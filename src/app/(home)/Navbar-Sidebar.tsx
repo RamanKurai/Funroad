ce9ec08c6f5d19b529496a93acbc866f218ddada
Raman Kurai <ramankurai27@gmail.com>
2025-07-11 19:50:26 +0530

    feat: create NavBarSidebar component and integrate it into Navbar for improved sidebar functionality

---
+++ src/app/(home)/Navbar-Sidebar.tsx
@@ -0,0 +1,47 @@
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet';
+import Link from 'next/link';
+import React from 'react'
+
+interface NavbarItems {
+    href : string,
+    children :  React.ReactNode
+}
+
+interface Props {
+    items : NavbarItems[],
+    open : boolean;
+    onOpenChange : (open : boolean) => void
+}
+
+const NavBarSidebar = ({items , open , onOpenChange} : Props) => {
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+       <SheetContent 
+       side='left'
+       className='p-0 transition-none'
+       >
+       <SheetHeader className='p-4 border-b'>
+         <div className='flex items-center'>
+            <SheetTitle>
+              Menu
+            </SheetTitle>
+         </div>
+       </SheetHeader>
+       <ScrollArea className='flex flex-col overflow-y-auto h-full pb-2' >
+         {items.map((item) => (
+           <Link 
+           key={item.href}
+           href={item.href}
+           className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+           >
+           {item.children}
+           </Link>
+         ))}
+       </ScrollArea>
+       </SheetContent>
+    </Sheet>
+)
+}
+
+export default NavBarSidebar
--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,54 +1,78 @@
-"use client"
-import React, { Children } from 'react'
-import Logo from './Logo'
-import NavbarItems from './NavbarItems'
-import { usePathname } from 'next/navigation'
-import { Button } from '@/components/ui/button'
+"use client";
+import React, { Children, useState } from "react";
+import Logo from "./Logo";
+import NavbarItems from "./NavbarItems";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import NavBarSidebar from "./Navbar-Sidebar";
+import {  MenuIcon } from "lucide-react";
 
 const navbarItems = [
-  {href: "/" , children: "Home"},
-  {href : "/about" , children : "About"},
-  {href : "/contact" , children : "Contact"},
-  {href : "/features" , children : "Features"},
-  {href : "/pricing" , children : "Pricing"},
-]
+  { href: "/", children: "Home" },
+  { href: "/about", children: "About" },
+  { href: "/contact", children: "Contact" },
+  { href: "/features", children: "Features" },
+  { href: "/pricing", children: "Pricing" },
+];
 
 const Navbar = () => {
-  const pathname = usePathname()
+  const pathname = usePathname();
+  const [isSidebarOpen , setIsSidebarOpen] = useState(true)
   return (
-    <nav className='flex justify-between items-center bg-black h-20'>
+    <nav className="flex justify-between items-center bg-black h-20">
       <div>
-        <Logo/>
+        <Logo />
       </div>
-      <div className='hidden lg:flex h-full items-center gap-4' >
-        <div className="flex items-center gap-4">
-        {navbarItems.map((items) => (
-       <NavbarItems
-       key={items.href}
-       href={items.href}
-       isActive={pathname === items.href}>
-      {items.children}
-      </NavbarItems>
-      ))}
-      </div>
-        <div className='hidden lg:flex h-full'>
-          <Button 
-          variant={'secondary'}
-          className='border-l-slate-50 h-full px-6 rounded-none text-white bg-transparent hover:bg-white hover:text-black transition-colors text-lg ' >
-          Log In
-        </Button>
-        <Button
-        variant={'secondary'}
-        className='h-full px-6 rounded-none text-black bg-pink-400 hover:bg-white hover:text-black transition-colors text-lg '
-        >
-          Start Selling
-        </Button>
+      <div className="hidden lg:flex h-full items-center gap-2">
+        <div className="flex items-center gap-2">
+          {navbarItems.map((items) => (
+            <NavbarItems
+              key={items.href}
+              href={items.href}
+              isActive={pathname === items.href}
+            >
+              {items.children}
+            </NavbarItems>
+          ))}
         </div>
-        </div>
-    </nav>
-      
-    
-  )
-}
 
-export default Navbar
+       
+        <div className="hidden lg:flex h-full">
+          <Button
+            
+            variant={"secondary"}
+            className="border-l-slate-50 h-full px-6 rounded-none text-white bg-transparent hover:bg-white hover:text-black transition-colors text-lg "
+          >
+            <Link href={"/sign-in"}>Log In</Link>
+          </Button>
+          <Button
+            
+            variant={"secondary"}
+            className="h-full px-6 rounded-none text-black bg-pink-400 hover:bg-white hover:text-black transition-colors text-lg "
+          >
+            <Link href={"/start-selling"}>Start Selling</Link>{" "}
+          </Button>
+        </div>
+      </div>
+
+      <NavBarSidebar
+      items={navbarItems}
+      open={isSidebarOpen}
+      onOpenChange={setIsSidebarOpen}
+      />
+
+      <div className="flex lg:hidden items-center justify-center">
+        <Button
+        variant={"ghost"}
+        className="pr-2 size-16 border-transparent bg-black hover:bg-black"
+        onClick={() => setIsSidebarOpen(true)}
+        >
+        <MenuIcon color="white" />
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
